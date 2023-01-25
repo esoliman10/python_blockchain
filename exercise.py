9ff2199b482cd029f436624828588f303b6d5b2b
Edson Soliman <engredson@gmail.com>
2023-01-25 20:06:11 +0800

    add exercise 2

--- exercise.py
+++ exercise.py
@@ -1,20 +1,36 @@
-name = "Kamote Smith"
-age = 20
+# #  Exercise 1: Create a function which prints the name and age of a person.
+# name = "Kamote Smith"
+# age = 20
 
-def print_data():
-    print(name + " is " + str(age) + " years old.")
+# def print_data():
+#     print(name + " is " + str(age) + " years old.")
 
-print_data()
+# print_data()
 
-def print_data(name, age):
-    print(name + " is " + str(age) + " years old.")
+# def print_data(name, age):
+#     print(name + " is " + str(age) + " years old.")
 
-print_data("Kamote Smith", 20)
-print_data("JR Smith", 30)
+# print_data("Kamote Smith", 20)
+# print_data("JR Smith", 30)
 
-# Create a function which calculates and returns the number of decades a person has lived.
-def calculate_decades(age):
-    decade = age // 10
-    print("You have lived for " + str(decade) + " decades.")
+# # Create a function which calculates and returns the number of decades a person has lived.
+# def calculate_decades(age):
+#     decade = age // 10
+#     print("You have lived for " + str(decade) + " decades.")
 
-calculate_decades(25)
+# calculate_decades(25)
+
+
+# #  Exercise 2: Create a
+# names = ["Ako toh", "Ikaw Yan","Patatas Kamoteque", "Lebron James", "Kevin Durant", "Stephen Curry", "Kawhi Leonard", "James Harden", "Giannis Antetokounmpo", "Paul George", "Nikola Jokic"]
+
+# for name in names:
+#     # print(len(name))
+#     if len(name) > 12 and ("n" in name or "N" in name):
+#             print(name)
+# print("-" * 20)
+
+# while len(names) >= 1:
+#     print(names.pop())
+
+# print(names)
